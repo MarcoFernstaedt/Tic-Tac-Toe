72983e3ed113a65ded0664a33e330f4edd23d0a8
Marco <fernstaedtmarco@gmail.com>
2022-09-10 14:28:38 -0700

    working on createPlayer factory functrion to assign marker and number working on the gameFlow how it will begin the game

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
-// gameboard module
-const GameBoard = (function () {
-    let gameBoard = []
-    return {};
-})
+const gameboard = (function () {
+    let game = ['X']
+    return { game };
+}())
 
-// display controller module
-const DisplayController (function () {
-    // come back later
-})
+const gameFlow = (function () {
+    let playerName1 = prompt('Please Enter Your Name: ')
+    let playerName2 = prompt('Please Enter Your Name: ')
+    if (playerNumber !== 2) {
+        let player1 = createPlayer(playerName1)
+    } else {
+        let player2 = createPlayer(playerName2)
+    }
 
-// Create player Factory Functrion
-const CreatePlayer = (playerName, playerNumber, playerXO) => {
-    return { playerName, playerNumber, playerXO }
+    return { playerName1.playerNumber, playerName2.playerNumber };
+}())
+
+function createPlayer(playerName, playerNumber, playerOX) {
+    let assignMark = ['X', 'O']
+    let assignNumber = [1, 2]
+    const playerOX = ''
+    const playerNumber = ''
+
+    if (playerName === '' || playerName == null) {
+        alert('Please enter a valid name!')
+        createPlayer(playerName)
+    } else if (playerName !== '' || playerName !== null) {
+        if (assignMark.length > 0) {
+            playerOX = assignMark[0]
+            playerNumber = assignNumber[0]
+            assignMark.shift()
+            assignNumber.shift()
+        }
+    }
+    return { playerName, playerNumber, playerOX }
+};
+
+function renderToScreen() {
+    let tiles = document.querySelectorAll('.tiles')
+    tiles.forEach((tile) => {
+        tile.addEventListener('click', () => {
+
+            tile.textContent = gameboard.game[0]
+        })
+    })
+    return { tiles }
 }
 
-const Marco = CreatePlayer('Marco', 1, 'X')
-const Grace = CreatePlayer('Grace', 2, 'O')
+renderToScreen()
