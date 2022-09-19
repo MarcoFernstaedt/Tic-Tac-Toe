7743b5eb22d00c904e2fa1292e1f4eeb0e150a89
Marco <fernstaedtmarco@gmail.com>
2022-09-19 12:03:27 -0700

    working on logic to switch whos turn it is

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
+const player1 = ['name', 'X']
+const player2 = ['name', 'O']
+
 const gameboard = (function () {
-    let game = ['X']
+    let game = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
     return { game };
 }())
 
 const gameFlow = (function () {
     createPlayer()
 
-    // return { player1, player2 }
-    // {
-    //     p1: player1.playerNumber,
-    //     p2: player2.playerNumber,
+    let currentPlayer = player1
+    let previousPlyaer = null;
+    // while (true) {
+    //     if (currentPlayer === player1) {
+    //         marker(currentPlayer[1])
+    //         previousPlyaer = currentPlayer
+    //         currentPlayer = player2
+    //     } else {
+    //         marker(currentPlayer[1])
+    //         previousPlyaer = createPlayer
+    //         currentPlayer = player1
+    //     }
     // }
+
+    return { currentPlayer }
 }())
 
 function createPlayer(playerName) {
     let name1 = prompt('Enter player1 name')
     let name2 = prompt('Enter player2 name')
 
-    const player1 = ['name', 'X']
     player1[0] = name1.toUpperCase()
-    const player2 = ['name', 'O']
     player2[0] = name2.toUpperCase()
 
-    console.log(player1, player2)
     return { player1, player2 }
 };
 
@@ -30,11 +40,18 @@ function renderToScreen() {
     let tiles = document.querySelectorAll('.tiles')
     tiles.forEach((tile) => {
         tile.addEventListener('click', () => {
-
-            tile.textContent = gameboard.game[0]
+            let index = tile.dataset.number
+            gameboard.game[index] = gameFlow.currentPlayer[1]
+            tile.textContent = gameboard.game[index]
         })
     })
     return { tiles }
 }
 
 renderToScreen()
+
+function marker(mark) {
+
+    return { mark }
+}
+
